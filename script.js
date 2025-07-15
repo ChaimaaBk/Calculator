738e2142bd555c76f17cccab8cc6e937bbd1e5a1
Chaimaa Boukniter <boukniter.chaimaa@gmail.com>
2025-07-15 09:47:38 +0100

    delete numbers

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let inputs = document.querySelectorAll('input');
 let display = document.querySelector('.display');
 let reset = document.querySelector('.reset');
+let del = document.querySelector('.delete');
 
 inputs.forEach(input =>{
     input.addEventListener('click',()=>{
         if (input === reset)
             display.value = display.value.toString().slice(-1,0);
+        else if (input === del)
+            display.value = display.value.toString().slice(0,-1);
         else
             display.value += input.value;
     })
 })
+
 
