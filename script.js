61aa07f147daa26925705dc6c9f9d988b9cc2822
Chaimaa Boukniter <boukniter.chaimaa@gmail.com>
2025-07-15 09:49:27 +0100

    eqal operations

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputs = document.querySelectorAll('input');
 let display = document.querySelector('.display');
 let reset = document.querySelector('.reset');
 let del = document.querySelector('.delete');
+let equal = document.querySelector('.equal');
 
 inputs.forEach(input =>{
     input.addEventListener('click',()=>{
@@ -9,9 +10,9 @@ inputs.forEach(input =>{
             display.value = display.value.toString().slice(-1,0);
         else if (input === del)
             display.value = display.value.toString().slice(0,-1);
+        else if (input === equal)
+            display.value = eval(display.value);
         else
             display.value += input.value;
     })
 })
-
-
