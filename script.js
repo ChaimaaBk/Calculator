af64b2ce29535370c0999a4a220a692b0eb0bc81
Chaimaa Boukniter <boukniter.chaimaa@gmail.com>
2025-07-15 09:45:29 +0100

    reset numbers

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let inputs = document.querySelectorAll('input');
 let display = document.querySelector('.display');
+let reset = document.querySelector('.reset');
 
 inputs.forEach(input =>{
     input.addEventListener('click',()=>{
-        
+        if (input === reset)
+            display.value = display.value.toString().slice(-1,0);
+        else
             display.value += input.value;
     })
 })
+
